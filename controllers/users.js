cac459c036c2a4a22e2666ddc897c35f3742e084
Dmitrii Chernyshev <chernyshevddi@yandex.ru>
2022-02-09 00:11:49 +0300

    добавил env

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 const NotFoundError = require('../utils/errors/notFound');
 const BadRequestError = require('../utils/errors/badRequest');
 const ConflictError = require('../utils/errors/conflict');
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports.getUserInfo = (req, res, next) => {
   User.findById(req.user._id)
@@ -43,7 +44,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,11 +2,11 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/unauthorized');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { NODE_ENV, JWT_SECRET } = process.env;
   let payload;
 
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(req.cookies.token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
